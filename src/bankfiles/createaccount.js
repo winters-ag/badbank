99e21201ab77641d719af87f3145f69f7b8f7e11
winters-ag <winters.alan.g@gmail.com>
2023-12-23 21:33:31 -0600

    fix formatting on createaccount file

--- src/bankfiles/createaccount.js
+++ src/bankfiles/createaccount.js
@@ -58,13 +58,13 @@ function CreateAccount() {
       status={status}
       body={show ? (
         <>
-        Name <br />
-        <input type="input" className="form-control" id="name" placeholder="Enter Name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-        Email Address<br/>
-        <input type="input" className="form-control" id="email" placeholder="Enter Email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br/>
-        Password<br />
-        <input type="password" className="form-control" id="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br/>
-        <button type="submit" className="btn btn-dark" onClick={handleCreate}>Create Account</button>
+          Name <br />
+          <input type="input" className="form-control" id="name" placeholder="Enter Name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+          Email Address<br/>
+          <input type="input" className="form-control" id="email" placeholder="Enter Email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br/>
+          Password<br />
+          <input type="password" className="form-control" id="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br/>
+          <button type="submit" className="btn btn-dark" onClick={handleCreate}>Create Account</button>
         </>
       ):(
         <>
